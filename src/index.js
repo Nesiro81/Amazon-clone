2ddd6ae61b425d0b131253ce647675838745885b
Nesiro81 <rejebnesiro81@gmail.com>
2024-12-01 23:49:32 +0100

    editindex

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-// import { reducer,initialState} from './Utility/Reducer';
-// import{DataProvider} from './components/DataProvider/DataProvider'
+ import { reducer,initialState} from './Utility/Reducer.js';
+ import{DataProvider} from './components/DataProvider/DataProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <DataProvider reducer={ reducer} initialState={initialState} > */}
+    <DataProvider reducer={ reducer} initialState={initialState} > 
     <App />
-    {/* </DataProvider> */}
+     </DataProvider> 
   </React.StrictMode>
 );
+
 
