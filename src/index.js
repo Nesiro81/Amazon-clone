264d52f1d9922271eac70c87be6060bcb4b51c3c
Nesiro81 <rejebnesiro81@gmail.com>
2024-12-02 01:55:02 +0100

    index

--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
-{/*import React from 'react';
-import ReactDOM from 'react-dom/client';
 
-import App from './App';
- import { reducer,initialState} from './Utility/Reducer.js';
- import{DataProvider} from './components/DataProvider/DataProvider'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <DataProvider reducer={ reducer} initialState={initialState} > 
-    <App />
-     </DataProvider> 
-  </React.StrictMode>
-);*/}
 
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
-import { reducer, initialState } from "./Utility/Reducer.js";
+ import { reducer, initialState } from "./Utility/Reducer.js";
 import { DataProvider } from "./components/DataProvider/DataProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/Amazon-clone">
-      <DataProvider reducer={reducer} initialState={initialState}>
+      <DataProvider reducer={reducer} initialStat={initialState}>
         <App />
       </DataProvider>
     </BrowserRouter>
